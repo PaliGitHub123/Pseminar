80acb188110ee605179f5d7d53524a63e901d477
PaliGitHub123 <146630889+PaliGitHub123@users.noreply.github.com>
2024-03-09 12:24:18 +0100

    loginseite dunkel

--- login.php
+++ login.php
@@ -1,8 +1,24 @@
 <html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initialt-scale=1">
+    <link rel="stylesheet" type="text/css" href="login_layout.css" />
+    <title> Login </title>
+</head>
+
 <body>
-	<h1>
-		Test Überschrift
-	</h1>
-	yoyoyo pal ich hätte gedacht du fängst schon an
+    <h1>
+        Login
+    </h1>
+
+    <form action="" method="POST">
+        <p>Name eingeben</p>
+        <input type="text" name ="login_name" class = "textbox"><br>
+        <p>Passwort eingeben</p>
+        <input type="password" name ="login_pass" class = "textbox"><br>
+        <input type="submit" value="Login" class = "submit">
+    </form>
+
 </body>
+
 </html>
